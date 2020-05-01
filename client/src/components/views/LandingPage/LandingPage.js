71d8fc700a554e9e7b583d990838a39f90303f84
ceylon85 <canleez123@gmail.com>
2020-05-01 18:50:42 +0900

    Landing

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,27 +1,89 @@
-import React ,{useEffect}from 'react'
+import React, {useEffect, useState} from 'react'
 import Axios from 'axios';
+import {
+    Icon,
+    Col,
+    Card,
+    Row,
+    Typography,
+    Button
+} from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider';
+
+const {Title} = Typography;
 
 function LandingPage() {
 
-    useEffect(() => {
-    
-        Axios.post('/api/product/products')
-        .then(response => {
-            if(response.data.success){
+    const [Products,
+        setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
 
-            }else{
-                alert('상품들을 가져오는데 실패 ㅠ ㅠ')
-            }
-        })
+    useEffect(() => {
+        let body = {
+            skip: Skip,
+            limit: Limit
+        }
+
+        Axios
+            .post('/api/product/products', body)
+            .then(response => {
+                if (response.data.success) {
+                    setProducts(response.data.productInfo)
+                } else {
+                    alert('상품들을 가져오는데 실패')
+                }
+            })
     }, [])
 
+    const loadmoreHandler = () => {
+          
+    }
+
+    const renderCards = Products.map((product, index) => {
+        return <Col key={index} lg={6} md={8} xs={24} >
+            <Card 
+            cover={< ImageSlider image={product.images} />}>
+                <Meta 
+                title={product.title} 
+                description={`$${product.price}`}/>
+            </Card>
+        </Col>
+    })
 
     return (
-        
-        <div>
-            landig
+
+        <div
+            style={{
+            width: '75%',
+            margin: '3rem auto'
+        }}>
+            <div style={{
+                textAlign: 'center'
+            }}>
+                <Title>Let`s Travel Anywhere!!!<Icon type="rocket"/></Title><hr/>
+            </div>
+
+            {/* Filter */}
+
+            {/* Search */}
+
+            {/* Cards */}
+            <Row gutter={[16, 16]}>
+                {renderCards}
+            </Row>
+            <br/>
+
+            <div
+                style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <Button onClick={loadmoreHandler}>더 보기</Button>
+            </div>
         </div>
-       
+
     )
 }
 
