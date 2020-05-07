ab89f62c8902d31fdb75adf4516bbc8ecdac63d2
ceylon85 <canleez123@gmail.com>
2020-05-07 17:51:19 +0900

    DetailPage

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -40,7 +40,7 @@ function DetailProductPage(props) {
 
                 <Col lg={12} sm={24}>
                 {/* ProductInfo */}
-                <ProductInfo />
+                <ProductInfo detail={Product} />
 
                 </Col>
             </Row>
--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Button, Descriptions} from 'antd'
 
-function ProductInfo() {
+function ProductInfo(props) {
+
+    const clickHandler=()=>{
+
+    }
+    
     return (
         <div>
-            information
+            <Descriptions title="상품 정보" bordered>
+                <Descriptions.Item label="가격">
+                {props.detail.price}
+                </Descriptions.Item>
+                <Descriptions.Item label="Sold">
+                {props.detail.sold}
+                    </Descriptions.Item>
+                    <Descriptions.Item label="View">
+                    {props.detail.views}
+                </Descriptions.Item>
+                <Descriptions.Item label="설명">
+                    {props.detail.description}
+                </Descriptions.Item>
+            </Descriptions>
+        
+        <br/>
+        <br/>
+        <br/>
+        <div style={{display:'flex', justifyContent:'center'}}>
+            <Button size="large" shape="round" type="danger" onClick={clickHandler}>장바구니 담기</Button>
+        </div>
         </div>
     )
 }
