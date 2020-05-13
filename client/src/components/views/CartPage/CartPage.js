191b186277ce01c1dc7db1e1831997a4f068c8ce
ceylon85 <canleez123@gmail.com>
2020-05-13 18:37:24 +0900

    Paypal

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,7 +2,8 @@ import React,{useEffect, useState} from 'react'
 import { useDispatch } from 'react-redux';
 import {getCartItems, removeCartItem} from '../../../_actions/user_actions';
 import UserCardBlock from './Sections/UserCardBlock';
-import {Typography} from 'antd';
+import {Typography, Empty} from 'antd';
+import Paypal from '../../utils/Paypal';
 
 const {Title} = Typography; 
 
@@ -10,6 +11,7 @@ function CartPage(props) {
 
     const dispatch = useDispatch();
     const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
 
     useEffect(() => {
         let cartItems = [];
@@ -38,12 +40,15 @@ function CartPage(props) {
         return total += parseInt(item.price, 10) * item.quantity
         })
         setTotal(total)
+        setShowTotal(true)
     }
 
     let removeFromCart = (productId) => {
         dispatch(removeCartItem(productId))
         .then(response => {
-
+            if(response.payload.productInfo.length <=0){
+                setShowTotal(false)
+            }
         })
     }
 
@@ -53,9 +58,18 @@ function CartPage(props) {
             <div>
             <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
             </div>
+
+            {ShowTotal ?
             <div style={{marginTop: '3rem'}}>
                   <h2>총 금액: $ {Total}</h2>
             </div>
+            :
+            <> 
+            <Empty description={false}/>
+            </>
+            }
+
+            <Paypal />
         </div>
     )
 }
